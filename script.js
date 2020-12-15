4f1387f0987556113074563aa84a8d327ecc9938
Abhyuday Tripathi <72795925+abhyuday-tripathi@users.noreply.github.com>
2020-12-15 15:25:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const titleChange = () => {
-    document.title = "TEST 2"
-}
+document.getElementById('btn').addEventListener('click', () => {
+    document.title = "Title  222222222222222222222222"
+});
